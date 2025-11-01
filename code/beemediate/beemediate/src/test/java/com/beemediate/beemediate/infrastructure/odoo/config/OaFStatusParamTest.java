f305bf89031bca923ff7a8e77d6f35dd63d46225
giorgio-hash <g.chirico1@studenti.unibg.it>
2025-11-01 14:12:13 +0100

    testing su OaFStatus

--- code/beemediate/beemediate/src/test/java/com/beemediate/beemediate/infrastructure/odoo/config/OaFStatusParamTest.java
+++ code/beemediate/beemediate/src/test/java/com/beemediate/beemediate/infrastructure/odoo/config/OaFStatusParamTest.java
@@ -13,21 +13,21 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class OaFStatusParamTest {
 	
-    private final OdooApiConfig.OafStatus status;
+    private final OafStatus status;
     private final String expectedLabel;
 
     @Parameters
     public static Collection<Object[]> parameters() {
         return Arrays.asList(
-                new Object []{ OdooApiConfig.OafStatus.NEW, "NEW" },
-                new Object []{ OdooApiConfig.OafStatus.SHIPPED, "SHIPPED" },
-                new Object []{ OdooApiConfig.OafStatus.CONFIRMED, "CONFIRMED" },
-                new Object []{ OdooApiConfig.OafStatus.OPENTRANSERROR, "OPENTRANSERROR" },
-                new Object []{ OdooApiConfig.OafStatus.CONTENTERROR, "CONTENTERROR" }
+                new Object []{ OafStatus.NEW, "NEW" },
+                new Object []{ OafStatus.SHIPPED, "SHIPPED" },
+                new Object []{ OafStatus.CONFIRMED, "CONFIRMED" },
+                new Object []{ OafStatus.OPENTRANSERROR, "OPENTRANSERROR" },
+                new Object []{ OafStatus.CONTENTERROR, "CONTENTERROR" }
         );
     }
 
-    public OaFStatusParamTest(OdooApiConfig.OafStatus status, String expectedLabel) {
+    public OaFStatusParamTest(OafStatus status, String expectedLabel) {
         this.status = status;
         this.expectedLabel = expectedLabel;
     }
