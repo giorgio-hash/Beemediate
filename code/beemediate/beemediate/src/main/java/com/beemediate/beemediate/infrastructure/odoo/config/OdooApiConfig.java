70809351d4bbf843f70a22281de654f6ae970e5e
giorgio-hash <g.chirico1@studenti.unibg.it>
2025-11-01 14:11:52 +0100

    refactoring

--- code/beemediate/beemediate/src/main/java/com/beemediate/beemediate/infrastructure/odoo/config/OdooApiConfig.java
+++ code/beemediate/beemediate/src/main/java/com/beemediate/beemediate/infrastructure/odoo/config/OdooApiConfig.java
@@ -110,34 +110,6 @@ public class OdooApiConfig {
 	 * flag per segnalare stato della connessione.
 	 */
 	private boolean online = false;
-	/**
-	 * Stato degli Ordini a Fornitore, ovvero gli Ordini di Acquisto di Odoo.
-	 */
-	public enum OafStatus{
-		NEW("NEW"),
-		SHIPPED("SHIPPED"),
-		CONFIRMED("CONFIRMED"),
-		OPENTRANSERROR("OPENTRANSERROR"),
-		CONTENTERROR("CONTENTERROR");
-		
-		/**
-		 * String corrispondente allo stato
-		 */
-		public final String label;
-		
-		/**
-		 * Costruttore privato
-		 * @param label - String
-		 */
-		OafStatus(final String label) {
-			this.label = label;
-		}
-		
-		@Override
-		public String toString() {
-			return label;
-		}
-	}
 	
 	
 	/**
