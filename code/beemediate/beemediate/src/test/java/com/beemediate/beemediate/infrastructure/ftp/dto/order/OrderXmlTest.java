235cfbf699c821eb6905867232f55c972206599c
giorgio-hash <g.chirico1@studenti.unibg.it>
2025-11-17 02:16:02 +0100

    test di conformità degli oggetti serializzati/deserializzati da Jackson rispetto ai template strutture dati

--- code/beemediate/beemediate/src/test/java/com/beemediate/beemediate/infrastructure/ftp/dto/order/OrderXmlTest.java
+++ code/beemediate/beemediate/src/test/java/com/beemediate/beemediate/infrastructure/ftp/dto/order/OrderXmlTest.java
@@ -1,29 +1,36 @@
 package com.beemediate.beemediate.infrastructure.ftp.dto.order;
 
-import com.beemediate.beemediate.infrastructure.ftp.dto.commons.XmlParty;
-import com.beemediate.beemediate.infrastructure.ftp.dto.confirmation.XmlOrderResponseInfo;
-import com.ctc.wstx.stax.WstxInputFactory;
-import com.ctc.wstx.stax.WstxOutputFactory;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.dataformat.xml.XmlFactory;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.InputStream;
+
+import javax.xml.stream.XMLInputFactory;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
-import java.io.InputStream;
-
-import javax.xml.stream.XMLInputFactory;
-
-import static org.junit.Assert.*;
+import com.beemediate.beemediate.infrastructure.ftp.dto.commons.XmlDeliveryDate;
+import com.beemediate.beemediate.infrastructure.ftp.dto.commons.XmlOrderPartiesReference;
+import com.beemediate.beemediate.infrastructure.ftp.dto.commons.XmlParty;
+import com.beemediate.beemediate.infrastructure.ftp.dto.commons.XmlProductID;
+import com.beemediate.beemediate.infrastructure.ftp.dto.commons.XmlShipmentPartiesReference;
+import com.ctc.wstx.stax.WstxInputFactory;
+import com.ctc.wstx.stax.WstxOutputFactory;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.dataformat.xml.XmlFactory;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 /**
  * Test sulla struttura dell'ORDER di esempio (ESEMPIO_CASO_REALE.xml) usando ResourceLoader.
  */
-public class OrderXmlStructureTest {
+public class OrderXmlTest {
 
     private XmlMapper mapper;
     private ResourceLoader resourceLoader;
@@ -47,108 +54,96 @@ public class OrderXmlStructureTest {
     }
 
     @Test
-    public void parseOrderAndCheckSummaryTotalItems() throws Exception {
+    public void validateRootAttributes() throws Exception {
+        Resource res = resourceLoader.getResource("classpath:xml/order/ESEMPIO_CASO_REALE.xml");
+        assertTrue("resource must exist in classpath: xml/order/ESEMPIO_CASO_REALE.xml", res.exists());
+
+        try (InputStream is = res.getInputStream()) {
+            JsonNode root = mapper.readTree(is);
+            // map genericamente in un oggetto Java (LinkedHashMap / Lists)
+            XmlOrder dto = mapper.treeToValue(root, XmlOrder.class);
+            assertNotNull("DTO after treeToValue should not be null", dto);
+            // guardo gli attributi di schema
+            assertEquals("XmlOrder has mapped xmlns", root.path("xmlns").toString().replaceAll("\"", ""), dto.getXmlns());
+            assertEquals("XmlOrder has mapped xsi", root.path("xmlns:xsi").toString().replaceAll("\"", ""), dto.getXsi());
+            assertEquals("XmlOrder has mapped bmecat", root.path("xmlns:bmecat").toString().replaceAll("\"", ""), dto.getBmecat());
+            assertEquals("XmlOrder has mapped version", root.path("version").toString().replaceAll("\"", ""), dto.getVersion());
+            assertEquals("XmlOrder has mapped type", root.path("type").toString().replaceAll("\"", ""), dto.getType());
+            assertEquals("XmlOrder has mapped schema_location", root.path("xsi:schemaLocation").toString().replaceAll("\"", ""), dto.getSchemaLocation());
+        }
+    }
+    
+    @Test
+    public void validateHeader() throws Exception {
+        Resource res = resourceLoader.getResource("classpath:xml/order/ESEMPIO_CASO_REALE.xml");
+        assertTrue("resource must exist in classpath: xml/order/ESEMPIO_CASO_REALE.xml", res.exists());
+
+        try (InputStream is = res.getInputStream()) {
+            JsonNode root = mapper.readTree(is);
+            JsonNode infoNode = root.path("ORDER_HEADER").path("ORDER_INFO");
+            XmlOrderInfo xoi = mapper.treeToValue(infoNode, XmlOrderInfo.class);
+            assertEquals(infoNode.path("ORDER_ID").toString().replaceAll("\"", ""), xoi.getOrderId());
+            assertEquals(infoNode.path("ORDER_DATE").toString().replaceAll("\"", ""), xoi.getOrderDate());
+            assertEquals(infoNode.path("bmecat:CURRENCY").toString().replaceAll("\"", ""), xoi.getCurrency());
+            JsonNode deliveryDateNode = infoNode.path("DELIVERY_DATE");
+            XmlDeliveryDate xdd = xoi.getDeliveryDate();
+            assertEquals(deliveryDateNode.path("type").toString().replaceAll("\"", ""), xdd.getType());
+            assertEquals(deliveryDateNode.path("DELIVERY_START_DATE").toString().replaceAll("\"", ""), xdd.getDeliveryStartDate());
+            assertEquals(deliveryDateNode.path("DELIVERY_END_DATE").toString().replaceAll("\"", ""), xdd.getDeliveryEndDate());
+            JsonNode partyNodeList = infoNode.path("PARTIES").path("PARTY");
+            assertTrue("PARTIES dev'essere un array", partyNodeList.isArray());
+            JsonNode party = partyNodeList.get(0);
+            XmlParty xp = mapper.treeToValue(party, XmlParty.class);
+            assertEquals(party.path("bmecat:PARTY_ID").path("").toString().replaceAll("\"", ""), xp.getPartyId().getPartyId());
+            assertEquals(party.path("bmecat:PARTY_ID").path("type").toString().replaceAll("\"", ""), xp.getPartyId().getType());
+            assertEquals(party.path("PARTY_ROLE").toString().replaceAll("\"", ""), xp.getPartyRole());
+            JsonNode orderPartiesReference = infoNode.path("ORDER_PARTIES_REFERENCE");
+            XmlOrderPartiesReference xopr = xoi.getOrderPartiesReference();
+            assertEquals(orderPartiesReference.path("bmecat:BUYER_IDREF").path("").toString().replaceAll("\"", ""), xopr.getBuyerIdRef().getPartyId());
+            assertEquals(orderPartiesReference.path("bmecat:BUYER_IDREF").path("type").toString().replaceAll("\"", ""), xopr.getBuyerIdRef().getType());
+            assertEquals(orderPartiesReference.path("bmecat:SUPPLIER_IDREF").path("").toString().replaceAll("\"", ""), xopr.getSupplierIdRef().getPartyId());
+            assertEquals(orderPartiesReference.path("bmecat:SUPPLIER_IDREF").path("type").toString().replaceAll("\"", ""), xopr.getSupplierIdRef().getType());
+            JsonNode shipmentPartiesReference = orderPartiesReference.path("SHIPMENT_PARTIES_REFERENCE");
+            XmlShipmentPartiesReference xspr = xopr.getShipmentPartiesRef();
+            assertEquals(shipmentPartiesReference.path("DELIVERY_IDREF").path("").toString().replaceAll("\"", ""), xspr.getDeliveryIdRef().getPartyId());
+            assertEquals(shipmentPartiesReference.path("DELIVERY_IDREF").path("type").toString().replaceAll("\"", ""), xspr.getDeliveryIdRef().getType());
+        }
+    }
+    
+    @Test
+    public void validateItem() throws Exception{
+    	
+        Resource res = resourceLoader.getResource("classpath:xml/order/ESEMPIO_CASO_REALE.xml");
+        assertTrue("resource must exist in classpath: xml/order/ESEMPIO_CASO_REALE.xml", res.exists());
+
+        try (InputStream is = res.getInputStream()) {
+            JsonNode root = mapper.readTree(is);
+            JsonNode itemNodeList = root.path("ORDER_ITEM_LIST").path("ORDER_ITEM");
+            assertTrue("ORDER_ITEM_LIST dev'essere un array", itemNodeList.isArray());
+            JsonNode itemNode = itemNodeList.get(0);
+            XmlItem xi = mapper.treeToValue(itemNode, XmlItem.class);
+            assertEquals(itemNode.path("QUANTITY").toString().replaceAll("\"", ""), String.valueOf(xi.getQuantity()).substring(0, 2));
+            assertEquals(itemNode.path("bmecat:ORDER_UNIT").toString().replaceAll("\"", ""), xi.getOrderUnit());
+            assertEquals(itemNode.path("LINE_ITEM_ID").toString().replaceAll("\"", ""), String.valueOf(xi.getLineItemId()));
+            JsonNode partyIDNode = itemNode.path("PRODUCT_ID");
+            XmlProductID xpid = xi.getProdId();
+            assertEquals(partyIDNode.path("bmecat:SUPPLIER_PID").toString().replaceAll("\"", ""), xpid.getSupplierId().getPartyId());
+            assertEquals(partyIDNode.path("bmecat:BUYER_PID").path("").toString().replaceAll("\"", ""), xpid.getBuyerId().getPartyId());
+            assertEquals(partyIDNode.path("bmecat:BUYER_PID").path("type").toString().replaceAll("\"", ""), xpid.getBuyerId().getType());
+            assertEquals(partyIDNode.path("bmecat:DESCRIPTION_SHORT").toString().replaceAll("\"", ""), xpid.getDescriptionShort());
+        }
+    }
+    
+    @Test
+    public void validateSummary() throws Exception {
         Resource res = resourceLoader.getResource("classpath:xml/order/ESEMPIO_CASO_REALE.xml");
         assertTrue("resource must exist in classpath: xml/order/ESEMPIO_CASO_REALE.xml", res.exists());
 
         try (InputStream is = res.getInputStream()) {
             JsonNode root = mapper.readTree(is);
             JsonNode summaryNode = root.path("ORDER_SUMMARY");
-            assertFalse("ORDER_SUMMARY must exist", summaryNode.isMissingNode());
-            XmlOrderSummary summary = mapper.treeToValue(summaryNode, XmlOrderSummary.class);
-            assertNotNull("deserialized XmlOrderSummary", summary);
-            assertEquals(8, summary.getTotalItemNum());
-
-            JsonNode itemList = root.path("ORDER_ITEM_LIST");
-            assertFalse("ORDER_ITEM_LIST must exist", itemList.isMissingNode());
-            JsonNode itemsNodeList = itemList.path("ORDER_ITEM");
-            assertFalse("ORDER_ITEM should exist", itemsNodeList.isMissingNode());
-            if (itemsNodeList.isArray()) {
-                assertEquals(8, itemsNodeList.size());
-                XmlItem item = null;
-                for(JsonNode itemNode : itemsNodeList) {
-                	item = mapper.treeToValue(itemNode, XmlItem.class);
-                	assertNotNull("deserialized XmlItem", item);
-                }
-            } else {
-                // fallback: se non è array, almeno 1 elemento
-                int count = 0;
-                for (JsonNode ignored : itemList) count++;
-                assertTrue("should have at least 1 ORDER_ITEM", count >= 1);
-                XmlItem item = null;
-                item = mapper.treeToValue(itemsNodeList, XmlItem.class);
-                assertNotNull("deserialized XmlItem", item);
-            }
-        }
-    }
-    
-    @Test
-    public void parseOrderAndCheckHeader() throws Exception {
-        Resource res = resourceLoader.getResource("classpath:xml/order/ESEMPIO_CASO_REALE.xml");
-        assertTrue("resource must exist in classpath: xml/order/ESEMPIO_CASO_REALE.xml", res.exists());
-
-        try (InputStream is = res.getInputStream()) {
-            JsonNode root = mapper.readTree(is);
-
-            JsonNode headerNode = root.path("ORDER_HEADER");
-            assertFalse("ORDER_HEADER must exist", headerNode.isMissingNode());
-            XmlOrderHeader header = mapper.treeToValue(headerNode, XmlOrderHeader.class);
-            assertNotNull("deserialized XmlOrderHeader", header);
-
-            JsonNode infoNode = headerNode.path("ORDER_INFO");
-            assertFalse("ORDER_INFO must exist", infoNode.isMissingNode());
-            XmlOrderInfo info = mapper.treeToValue(infoNode, XmlOrderInfo.class);
-            assertNotNull("deserialized XmlOrderInfo", info);
-
-            JsonNode orderId = infoNode.path("ORDER_ID");
-            assertFalse("ORDER_ID must exist", orderId.isMissingNode());
-            assertEquals("ORDER_ID correctly mapped", info.getOrderId(), orderId.asText().trim());
-
-            JsonNode orderDate = infoNode.path("ORDER_DATE");
-            assertFalse("ORDER_DATE must exist", orderDate.isMissingNode());
-            assertEquals("ORDER_DATE correctly mapped", info.getOrderDate() , orderDate.asText().trim());
-
-            JsonNode currency = infoNode.path("bmecat:CURRENCY");
-            assertFalse("bmecat:CURRENCY must exist under ORDER_INFO", currency.isMissingNode());
-            assertEquals("bmecat:CURRENCY correctly mapped", info.getCurrency(), currency.asText().trim());
-
-            // verify parties exist and at least one PARTY is present
-            JsonNode parties = infoNode.path("PARTIES");
-            assertFalse("PARTIES must exist", parties.isMissingNode());
-            JsonNode partyNodeList = parties.path("PARTY");
-            assertFalse("PARTIES not empty", partyNodeList.isMissingNode());
-            assertTrue("PARTIES is an array", partyNodeList.isArray());
-            XmlParty party = null;
-            for(JsonNode node : partyNodeList) {
-            	party = mapper.treeToValue(node, XmlParty.class);
-            	assertNotNull("deserialized XmlParty", node);
-            }
-        }
-    }
-
-    @Test
-    public void ParseOrderAndCheckAttributes() throws Exception {
-        Resource res = resourceLoader.getResource("classpath:xml/order/ESEMPIO_CASO_REALE.xml");
-        assertTrue("resource must exist in classpath: xml/order/ESEMPIO_CASO_REALE.xml", res.exists());
-
-        try (InputStream is = res.getInputStream()) {
-            JsonNode original = mapper.readTree(is);
-            // map genericamente in un oggetto Java (LinkedHashMap / Lists)
-            XmlOrder dto = mapper.treeToValue(original, XmlOrder.class);
-            assertNotNull("DTO after treeToValue should not be null", dto);
-            // guardo gli attributi di schema
-            assertEquals("XmlOrder has mapped xmlns", original.path("xmlns").toString().replaceAll("\"", ""), dto.getXmlns());
-            assertEquals("XmlOrder has mapped xsi", original.path("xmlns:xsi").toString().replaceAll("\"", ""), dto.getXsi());
-            assertEquals("XmlOrder has mapped bmecat", original.path("xmlns:bmecat").toString().replaceAll("\"", ""), dto.getBmecat());
-            assertEquals("XmlOrder has mapped version", original.path("version").toString().replaceAll("\"", ""), dto.getVersion());
-            assertEquals("XmlOrder has mapped type", original.path("type").toString().replaceAll("\"", ""), dto.getType());
-            assertEquals("XmlOrder has mapped schema_location", original.path("xsi:schemaLocation").toString().replaceAll("\"", ""), dto.getSchemaLocation());
-
-            // serializza di nuovo in JsonNode e confronta strutturalmente
-            //JsonNode reserialized = mapper.valueToTree(dto);
-            //assertNotNull("reserialized JsonNode should not be null", reserialized);
-
-            //assertEquals("Round-trip structural equality for ORDER", original, reserialized);
+            XmlOrderSummary sum = mapper.treeToValue(summaryNode, XmlOrderSummary.class);
+            assertEquals(summaryNode.path("TOTAL_ITEM_NUM").toString().replaceAll("\"", ""), String.valueOf(sum.getTotalItemNum()));
         }
     }
 }
